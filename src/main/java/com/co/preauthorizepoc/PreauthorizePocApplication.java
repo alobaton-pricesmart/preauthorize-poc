83490233763fa14debdb02bfc8b1b7d4031ab112
Alvaro Jose Lobaton Restrepo <alobaton@itcol.com>
2019-04-12 13:45:08 -0500

    Initial commit

---
+++ src/main/java/com/co/preauthorizepoc/PreauthorizePocApplication.java
@@ -0,0 +1,13 @@
+package com.co.preauthorizepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreauthorizePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreauthorizePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/preauthorizepoc/PreauthorizePocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.co.preauthorizepoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PreauthorizePocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
